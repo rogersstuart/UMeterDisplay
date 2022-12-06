c34fd713e57e66c52d237cf7b55c45cbd3da3a69
rogersstuart <rogersstuart@gmail.com>
2022-12-05 22:22:46 -0500

    cleanup

--- Core/Src/lcd_if.c
+++ Core/Src/lcd_if.c
@@ -1,32 +1,5 @@
 #include "lcd_if.h"
 
-void lcd_idle(){
-	unsigned char flag=0xff;
-
-	GPIOB->ODR &= ~((uint16_t)1 << LCD_A0);
-	GPIOB->ODR |= ((uint16_t)1 << LCD_READ_EN);
-	GPIOB->ODR |= ((uint16_t)1 << LCD_WRITE_EN);
-
-	uint32_t moder_bak = GPIOB->MODER;
-
-	GPIOB->MODER &= ~((uint32_t)0xFFFF);
-
-	while ((flag&0x80)==0x80)
-	{
-			GPIOB->ODR &= ~((uint16_t)1 << LCD_READ_EN);
-			HAL_Delay(1);
-		    flag=(uint8_t)(GPIOB->IDR);
-		    GPIOB->ODR |= ((uint16_t)1 << LCD_READ_EN);
-	}
-
-
-	GPIOB->MODER = moder_bak;
-
-		return;
-
-
-}
-
 void lcd_init(){
 
 	GPIOB->ODR |= ((uint16_t)1 << LCD_READ_EN);
@@ -38,97 +11,93 @@ void lcd_init(){
 	GPIOB->ODR |= ((uint16_t)1 << LCD_CS);
 
 	asm("NOP");
-		asm("NOP");
-		asm("NOP");
-		asm("NOP");
-		asm("NOP");
-		asm("NOP");
-		asm("NOP");
-		asm("NOP");
-		asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
 
 	GPIOB->ODR |= ((uint16_t)1 << LCD_RST);
 	asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+
 	GPIOB->ODR &= ~((uint16_t)1 << LCD_RST);
 	asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+
 	GPIOB->ODR |= ((uint16_t)1 << LCD_RST);
 	asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
 
 	GPIOB->ODR &= ~((uint16_t)1 << LCD_CS);
 	asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-			asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-				asm("NOP");
-
-
-
-
-
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
+	asm("NOP");
 
 	//write 0xA2 10100010
 	lcd_write(0, 0xA2);
@@ -157,15 +126,7 @@ void lcd_init(){
 	//0x2F
 	lcd_write(0, 0x2F);
 
-
-	//lcd_write(0,0b10100111);
-
-	//
-
-
-
 	uint8_t page_addr = 0xD0;
-	//lcd_write(0,0xB0); //page 0 command
 
 	for(int y = 0; y < 8; y++)
 	{
@@ -182,79 +143,38 @@ void lcd_init(){
 			gpio_data |= 0x00;
 			GPIOB->ODR = gpio_data;
 			asm("NOP");
-					asm("NOP");
-					asm("NOP");
-					asm("NOP");
-					asm("NOP");
-					asm("NOP");
-					asm("NOP");
-					asm("NOP");
-					asm("NOP");
-						asm("NOP");
-						asm("NOP");
-						asm("NOP");
-						asm("NOP");
-						asm("NOP");
-						asm("NOP");
-						asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
+			asm("NOP");
 
 			GPIOB->ODR |= ((uint16_t)1 << LCD_WRITE_EN);
-			//lcd_write(1,0xff);
-
-
 		}
-		//page_addr+=1;
 
 		lcd_write(0,0xA4); //all pixels on (normal display)
-			lcd_write(0,0xAF); //display on
+		lcd_write(0,0xAF); //display on
 	}
-
-	//
-
-
-
-	//lcd_write(0, 0b10101110);
-
-
 }
-
-/*
-void setA0(uint8_t val){
-	if(val)
-		GPIOB->ODR |= ((uint16_t)1 << LCD_A0);
-	else
-		GPIOB->ODR &= ~((uint16_t)1 << LCD_A0);
-}
-*/
 
 void lcd_write(uint8_t cmd_dat, uint8_t data)
 {
-	//lcd_idle();
-
-	//A0 H = data L = cmd
-	//LCD port is B
-
-/*
-	GPIOB->ODR |= ((uint16_t)1 << LCD_READ_EN);
-	HAL_Delay(1);
-	GPIOB->ODR &= ~((uint16_t)1 << LCD_READ_EN);
-	//for the 8080 parallel interface ERD should be high when writing to the display
-	HAL_Delay(1);
-	GPIOB->ODR |= ((uint16_t)1 << LCD_READ_EN);
-	*/
-
 	//for the 8080 parallel interface RWR should be low when writing to the display
-
 
 	if(cmd_dat)
 		GPIOB->ODR |= ((uint16_t)1 << LCD_A0);
 	else
 		GPIOB->ODR &= ~((uint16_t)1 << LCD_A0);
-
-	//HAL_Delay(1);
-
-
-
 
 	//signals will be latched on the rising edge of wr
 	//when rd is low data lines are in output mode
@@ -267,9 +187,6 @@ void lcd_write(uint8_t cmd_dat, uint8_t data)
 	GPIOB->ODR = gpio_data;
 
 
-
-		//HAL_Delay(1);
-	//HAL_Delay(1);
 	asm("NOP");
 	asm("NOP");
 	asm("NOP");
@@ -278,9 +195,6 @@ void lcd_write(uint8_t cmd_dat, uint8_t data)
 	asm("NOP");
 	asm("NOP");
 	asm("NOP");
-
 
 	GPIOB->ODR |= ((uint16_t)1 << LCD_WRITE_EN);
-
-	//HAL_Delay(1);
 }
